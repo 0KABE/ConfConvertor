bd82aa1b0179dc612521cd9ddc7c50502685d6a3
0KABE <36717010+0KABE@users.noreply.github.com>
2019-03-29 22:31:54 +0800

    Add Surge3 Convert Support

--- xmlConvertor.py
+++ xmlConvertor.py
@@ -1,67 +1,117 @@
 import xml.etree.ElementTree as ET
 
-general_KeyWords = {"Surge3": ["allow-wifi-access", "dns-server", "ipv6", "loglevel", "skip-proxy", "use-default-policy-if-wifi-not-primary",
-                               "proxy-settings-interface", "always-real-ip", "hijack-dns", "tun-excluded-routes", "tun-included-routes"]}
-replica_KeyWords = {"Surge3": [
-    "hide-apple-request", "hide-crashlytics-request", "hide-udp", "use-keyword-filter"]}
-rule_KeyWords = {"Surge3": ["DOMAIN", "DOMAIN-SUFFIX", "DOMAIN-KEYWORD", "IP-CIDR", "GEOIP", "USER-AGENT",
-                            "URL-REGEX", "PROCESS-NAME", "AND", "OR", "NOT", "DEST-PORT", "SRC-IP", "IN-PORT", "RULE-SET", "FINAL"]}
+
+def Surge3(root):
+    General = "[General]\n"
+    for child in root.find("General"):
+        Default_Keywords = {"ipv6": "false", "loglevel":
+                            "notify", "allow-wifi-access": "false", "show-error-page-for-reject": "false"}
+        tag = child.tag
+        if tag in Default_Keywords:
+            General += tag+" = "+child.get("value", Default_Keywords[tag])+"\n"
+        elif tag == "skip-proxy":
+            General += tag+" = "
+            temp = list()
+            for child in child.findall("Item"):
+                temp.append(child.text)
+            General += ", ".join(temp)+"\n"
+        elif tag == "dns-server":
+            General += tag+" = "
+            temp = list()
+            for child in child.findall("Item"):
+                temp.append(child.text)
+            General += ", ".join(temp)+"\n"
+
+    Replica = "[Replica]\n"
+    for child in root.find("Replica"):
+        Default_Keywords = {"hide-apple-request": "true",
+                            "hide-crashlytics-request": "true", "hide-udp": "false"}
+        tag = child.tag
+        if tag in Default_Keywords:
+            Replica += tag+" = "+child.text+"\n"
+
+    Proxies = "[Proxy]\n"
+    for child in root.find("Proxies"):
+        tag = child.tag
+        if tag == "Built-in":
+            Proxies += child.get("key")+" = "+child.get("value")+"\n"
+
+    ProxyGroup = "[Proxy Group]\n"
+    for child in root.find("PolicyGroup"):
+        PolicyType = child.get("type")
+        ProxyGroup += child.get("name")+" = "
+        temp = list()
+        temp.append(PolicyType)
+        for child in child.iter("policy-path"):
+            temp.append(child.tag+" = "+child.text)
+        for child in child.iter("policy"):
+            temp.append(child.text)
+        if PolicyType == "fallback":
+            temp.append("url = " +
+                        child.get("url", "http://www.gstatic.com/generate_204"))
+            temp.append(child.get("timeout", "5"))
+        elif PolicyType == "url-test":
+            temp.append("url = " +
+                        child.get("url", "http://www.gstatic.com/generate_204"))
+            temp.append("timeout = "+child.get("timeout", "5"))
+            temp.append("tolerance = "+child.get("tolerance", "100"))
+            temp.append("interval = "+child.get("interval", "600"))
+        ProxyGroup += ", ".join(temp)+"\n"
+
+    Rule = "[Rule]\n"
+    for child in root.find("Rules"):
+        KeyWords = ["USER-AGENT", "DOMAIN-SUFFIX", "DOMAIN-KEYWORD", "GEOIP", "IP-CIDR", "USER-AGENT",
+                    "URL-REGEX", "PROCESS-NAME", "AND", "OR", "NOT", "DEST-PORT", "SRC-IP", "IN-PORT", "RULE-SET", "FINAL"]
+        if child.tag in KeyWords:
+            if child.tag == "FINAL":
+                Rule += child.tag+", "+child.get("policy")
+                if "dns-failed" in child.attrib and child.get("dns-failed") == "true":
+                    Rule += ", dns-failed"
+            else:
+                Rule += child.tag+", " + \
+                    child.get("value")+", "+child.get("policy")
+
+            if "notification-text" in child.attrib:
+                Rule += ", notification-text = \"" + \
+                    child.get("notification-text")+"\""
+            if "notification-interval" in child.attrib:
+                Rule += ", notification-interval = " + \
+                    child.get("notification-interval")
+            Rule += "\n"
+
+    Host = "[Host]\n"
+    for child in root.find("Host"):
+        if child.tag == "Server":
+            Host += child.get("key")+" = server:"+child.get("value")+"\n"
+
+    UrlRewrite = "[URL Rewrite]\n"
+    for child in root.find("UrlRewrite"):
+        UrlRewrite += child.get("expression")+" " + \
+            child.get("replacement")+" "+child.get("type")+"\n"
+
+    HeaderRewrite = "[Header Rewrite]\n"
+    for child in root.find("HeaderRewrite"):
+        HeaderRewrite += child.get("field")+" " + \
+            child.get("type") + " " + child.get("value")+"\n"
+
+    Mitm = "[MITM]\n"
+    Mitm += "enable = " + root.find("MITM").get("enable")+"\n"
+    Mitm += "skip-server-cert-verify = " + \
+        root.find("MITM").get("skip-server-cert-verify")+"\n"
+    Mitm += "ca-passphrase = " + root.find("MITM/ca-passphrase").text+"\n"
+    Mitm += "hostname = "
+    l = list()
+    for item in root.find("MITM/hostname"):
+        l.append(item.text)
+    Mitm += ", ".join(l)+"\n"
+
+    return General+Replica+Proxies+ProxyGroup+Rule+UrlRewrite+HeaderRewrite+Mitm
 
 
-def Surge3Pro(tree):
-    #get [General]
-    general_elem = tree.find("General")
-    general_str = "[General]\n"
-    for elem in general_elem.iter():
-        if elem.tag in general_KeyWords["Surge3"]:
-            general_str += elem.tag+" = "+elem.text+"\n"
+f = open("Private_Demo.xml", "r", encoding="utf-8")
+data = f.read()
+root = ET.fromstring(data)
+f = open("test.conf", "w", encoding="utf-8")
+f.write(Surge3(root))
 
-    #get [Replica]
-    replica_elem = tree.find("Replica")
-    replica_str = "\n[Replica]\n"
-    for elem in replica_elem.iter():
-        if elem.tag in replica_KeyWords["Surge3"]:
-            replica_str += elem.tag+" = "+elem.text+"\n"
-
-    #get [Proxy]
-    proxy_elem = tree.find("Proxy")
-    proxy_str = "\n[Proxy]\n"
-    for elem in proxy_elem:
-        proxy_str += elem.text+" = "+elem.tag+"\n"
-
-    # get [Proxy Group]
-    proxy_group_elem = tree.find("Proxy_Group")
-    proxy_group_str = "\n[Proxy Group]\n"
-    for elem in proxy_group_elem.iter("Group"):
-        proxy_group_str += elem.get("name")+" = "+elem.get("type")
-        for i in elem.iter("policy-path"):
-            proxy_group_str += ", "+i.tag+" = "+i.get("url")
-        for i in elem.iter("proxies"):
-            proxy_group_str += ", "+i.text
-        if elem.get("type") != "select":
-            if(elem.get("url")):
-                proxy_group_str += ", url = "+elem.get("url")
-            if elem.get("interval"):
-                proxy_group_str += ", interval = "+elem.get("interval")
-            if elem.get("tolerance"):
-                proxy_group_str += ", tolerance = "+elem.get("tolerance")
-            if elem.get("timeout"):
-                proxy_group_str += ", timeout = "+elem.get("timeout")
-        proxy_group_str += "\n"
-    #get [Rule]
-    rule_elem = tree.find("Rule")
-    rule_str = "\n[Rule]\n"
-    for elem in rule_elem:
-        if elem.tag in rule_KeyWords["Surge3"]:
-            rule_str += elem.tag+", "+elem.text + \
-                ", "+elem.get("proxy_group")+"\n"
-
-    ##
-    print(general_str+replica_str+proxy_str+proxy_group_str+rule_str)
-    return general_str+replica_str+proxy_str+proxy_group_str+rule_str
-
-
-f = open("Demo.xml", "r", encoding="utf-8")
-tree = ET.fromstring(f.read())
-
-conf = Surge3Pro(tree)
+# print(Surge3(root))
